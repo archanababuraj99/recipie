3e38b70b2f2934c43687224c563e1c43bbdb9dc6
archanababuraj99 <mca509@rajagiri.edu>
2021-03-16 16:17:18 +0530

    recipie

---
+++ about.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-success bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img height="55px" src="https://previews.123rf.com/images/tigatelu/tigatelu1311/tigatelu131100120/23826071-chef-cartoon-holding-platter.jpg" alt="" width="30" height="24" class="d-inline-block align-top">
+            TASTY KITCHEN
+          </a>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="index.php">Home</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="gallery.php">OUR RECIPIES</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="register.php">NEW REGISTER</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="about.php">ABOUT US</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contact.php">CONTACT US</a>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+      </nav>
+      <div class="container">
+          <div class="row">
+              <div class="col col-12 col-sm-6 col-lg-6">
+                  <img height="500px" src="https://previews.123rf.com/images/tigatelu/tigatelu1311/tigatelu131100120/23826071-chef-cartoon-holding-platter.jpg"
+              <div class="col col-12 col-sm-6 col-lg-6">
+                  <h2><center>OUR STORY</center></h2>
+                  <p><i>Since our modest beginnings in 2005 with a little space in Toronto’s stylish Yorkville locale, Archies Kitchen ‘s development has been enlivened with the energy to cook and serve solid, Indian-roused takeout food. In contrast to other Indian eateries, Archies Kitchen was made with the explicit expectation to appear as something else. We realize numerous individuals love Indian sustenance, yet a large number of them loathe or are unconscious of the regularly unfortunate fixings that make run of the mill Indian nourishment taste so great. Our menu highlights things that utilization the sound and fragrant flavors, however forgets the stuffing ghee, spread, oil, and overwhelming cream.</i></p>
+                  <p><i><span style="font-family: Verdana, Geneva, Tahoma, sans-serif;">"We bring you the best and soul touched recipies"</span></i></p>
+              </div>
+          </div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script> 
+</body>
+</html>
